4f8ee2c98149528427b12fc6b83b0ea6092f94f7
kebin2410 <kebinsteve@gmail.com>
2023-12-04 15:14:51 +0900

    commit 10

--- config/ThemeChange/Switch.js
+++ config/ThemeChange/Switch.js
@@ -6,7 +6,7 @@ import ThemeContext from "./ThemeContext";
 const Switch = (props) => {
   const { dark, toggle } = useContext(ThemeContext);
   return (
-    <div>
+    <div className="mx-2">
       <button className="Switch" onClick={() => toggle()}>
         {!dark ? (
           <Sun className={`icon ${!dark ? "active" : ""}`} />
