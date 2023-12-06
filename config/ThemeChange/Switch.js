1bec18d78ca20981b6ca7bc8dc50a903da2d4171
kebin2410 <kebinsteve@gmail.com>
2023-12-06 18:10:53 +0900

    commit 20

--- config/ThemeChange/Switch.js
+++ config/ThemeChange/Switch.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 // import { IoMdMoon as Moon, IoMdSunny as Sun } from "react-icons/io";
-import { LuSun as Moon, LuMoon as Sun } from "react-icons/lu";
+import { LuSun as Sun, LuMoon as Moon } from "react-icons/lu";
 import ThemeContext from "./ThemeContext";
 
 const Switch = () => {
@@ -8,7 +8,7 @@ const Switch = () => {
   return (
     <div className="mx-2">
       <button className="Switch" onClick={() => toggle()}>
-        {dark ? <Sun className="change-mode"/> : <Moon className="change-mode"/>}
+        {!dark ? <Sun strokeWidth="3px" size="3em" className="change-mode" /> : <Moon strokeWidth="3px" size="3em" className="change-mode" />}
       </button>
     </div>
   );
