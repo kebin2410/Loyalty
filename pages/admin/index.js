8e03b0ff838ab55314834ddaff6dd6676b5453c4
kebin2410 <kebinsteve@gmail.com>
2023-12-11 06:26:43 +0900

    05

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,12 +1,13 @@
 import Navbar from "../../components/navBar/Navbar";
 import AdminDashboard from "../../components/main/admin/admin";
+import { AdminMenuInfo } from "../../components/navBar/userMenu/userMenuData";
 import { navData } from "../../components/navBar/miniData/admin";
 import MinimalBar from "../../components/navBar/minimalBar";
 import Footer from "../../components/footer/Footer";
 export default function Dashboard() {
     return (
         <>
-            <Navbar />
+            <Navbar userMenu={AdminMenuInfo}/>
             <MinimalBar items={navData}/>
             <AdminDashboard />
             <Footer />
